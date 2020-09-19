d808e96aa171cf1c1f6198273757fd9f7d02cba9
Lynette <lynetteacholah@icloud.com>
2020-09-19 15:29:29 +0300

    Done

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ addToDoButton.addEventListener('click', function(){
   var paragraph = document.createElement('p');
   paragraph.innerText = inputField.value;
   toDoContainer.appendChild(paragraph);
+  inputField.value = ''
+  paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration = 'line-through';
+  paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+    })
+  })
 })
